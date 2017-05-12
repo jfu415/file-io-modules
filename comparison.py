53ef98b4e523d9f07f0503422932ae83c3f87757
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-11 21:07:00 -0700

    practice

--- comparison.py
+++ comparison.py
@@ -5,15 +5,21 @@ It should be used with the two input files, fruits_1.txt and fruits_2.txt."""
 def open_and_read_file(filename):
     """Opens file as a file object and returns list of contents."""
 
-    # Write your code below.
+    file_list = open(filename)
+    #file_list_print = file_list.read()
+    #print file_list_print
+    newlist = []
+    for item in file_list:
+        newlist.append(item)
+    print newlist
+    file_list.close()
 
 
-    pass
 
 
 def compare(lst1, lst2):
     """Takes in two lists and returns a list of items in common. """
-
+   
     # Write your code below.
 
 
@@ -21,3 +27,4 @@ def compare(lst1, lst2):
 
 
 # Call your functions at the bottom, after they've been defined.
+open_and_read_file("fruits_1.txt")
