2de1ff16864ee9e1f14cec2367d99bd69adfb96b
Hackbright Student <no-reply@hackbrightacademy.com>
2017-05-11 20:15:06 -0700

    initial commit

---
+++ comparison.py
@@ -0,0 +1,23 @@
+""" This file is part of the File I/O exercise.
+
+It should be used with the two input files, fruits_1.txt and fruits_2.txt."""
+
+def open_and_read_file(filename):
+    """Opens file as a file object and returns list of contents."""
+
+    # Write your code below.
+
+
+    pass
+
+
+def compare(lst1, lst2):
+    """Takes in two lists and returns a list of items in common. """
+
+    # Write your code below.
+
+
+    pass
+
+
+# Call your functions at the bottom, after they've been defined.
